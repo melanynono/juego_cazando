121e42d4bc062d042b4329c3d72ef11f4cb39524
melanynono <melanynono123@gmail.com>
2026-06-24 18:44:55 -0500

    agregar graficarComida y modificado de izquierda a derecha

--- cazando.js
+++ cazando.js
@@ -2,16 +2,31 @@ let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
 
 const ANCHO_GATO = 50;
-const ALTO_GATO = 70;
+const ALTO_GATO = 50;
+const ANCHO_COMIDA = 30;
+const ALTO_COMIDA = 30;
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
 
-let gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
-let gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
+function iniciarJuego(){
+    gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
+    gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
+    // Comida esquina inferior derecha
+    comidaX = canvas.width - ANCHO_COMIDA;
+    comidaY = canvas.height - ALTO_COMIDA;
 
-function iniciar(){
     graficarGato();
+    graficarComida();
 }
 
 function graficarGato(){
     ctx.fillStyle = "orange";
-    ctx.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
+    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO);
+}
+
+function graficarComida(){
+    ctx.fillStyle = "green";
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);
 }
